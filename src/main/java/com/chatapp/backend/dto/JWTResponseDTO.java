991783457cf21fa70cda734a130f36008448d793
anuragcs <anuraggupta@ninjacart.com>
2025-12-14 18:53:22 +0530

    add JWTResponseDTO for handling JWT response data

---
+++ src/main/java/com/chatapp/backend/dto/JWTResponseDTO.java
@@ -0,0 +1,13 @@
+package com.chatapp.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JWTResponseDTO {
+    private String token;
+    private String email;
+}
